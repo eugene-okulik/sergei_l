c98d5a88b6400f1cdb16e1e91f11ffbc1636c06a
humansinc1 <humans.inc@gmail.com>
2025-07-13 19:48:34 +0300

    added 4 files with tasks

---
+++ homework/sergei_l/homework_3/homework_3_1.py
@@ -0,0 +1,10 @@
+number_a = 2
+number_b = 31
+
+sum_numbers = number_a + number_b
+subtraction_numbers = number_a - number_b
+multiplication_numbers = number_a * number_b
+
+print(f"Сумма двух заданных чисел: {sum_numbers}")
+print(f"Разность двух заданных чисел: {subtraction_numbers}")
+print(f"Произведение двух заданных чисел: {multiplication_numbers}")
---
+++ homework/sergei_l/homework_3/homework_3_3.py
@@ -0,0 +1,9 @@
+first_number = 3
+second_number = 33
+qty_numbers = 2
+
+arithmetic_mean = (first_number + second_number) / qty_numbers
+geometric_mean = pow(first_number * second_number, 1/qty_numbers)
+
+print(f"Cреднее арифметическое заданных чисел: {arithmetic_mean}")
+print(f"Cреднее геометрическое заданных чисел: {geometric_mean}")
