58671ab1ef26a2eb17af6ac5e22f50c93d0b5d1e
humansinc1 <humans.inc@gmail.com>
2025-07-13 19:52:31 +0300

    fixed lint errors

--- homework/sergei_l/homework_3/homework_3_4.py
+++ homework/sergei_l/homework_3/homework_3_4.py
@@ -1,7 +1,7 @@
 leg_a = 24.5
 leg_b = 33.4
 
-hypotenuse = pow(leg_a ** 2 + leg_b ** 2, 1/2)
+hypotenuse = pow(leg_a ** 2 + leg_b ** 2, 1 / 2)
 area = leg_a * leg_b / 2
 
 print(f"Гипотенуза треугольника: {hypotenuse}")
