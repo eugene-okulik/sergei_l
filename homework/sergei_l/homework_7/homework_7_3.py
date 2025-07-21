5360b5a880d5958cc00ec0a5204c4125e7fb012d
humansinc1 <humans.inc@gmail.com>
2025-07-21 13:03:02 +0300

    fixed lint issue

--- homework/sergei_l/homework_7/homework_7_3.py
+++ homework/sergei_l/homework_7/homework_7_3.py
@@ -3,10 +3,13 @@ string2 = 'результат операции: 54'
 string3 = 'результат работы программы: 209'
 string4 = 'результат: 2'
 
+
 def result_calc(string):
     return int(string.split()[-1]) + 10
+
 
 print(result_calc(string1))
 print(result_calc(string2))
 print(result_calc(string3))
 print(result_calc(string4))
+
