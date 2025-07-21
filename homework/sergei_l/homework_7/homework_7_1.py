3078e3de8c4e01d26d0eedc2f9676bf4065d6259
humansinc1 <humans.inc@gmail.com>
2025-07-21 12:58:13 +0300

    added 3 tasks for homework 7

---
+++ homework/sergei_l/homework_7/homework_7_1.py
@@ -0,0 +1,7 @@
+number = 5
+user_number = int(input('Введите цифру: '))
+
+while user_number != number:
+    user_number = int(input('Попробуйте снова: '))
+
+print('Поздравляю! Вы угадали!')
---
+++ homework/sergei_l/homework_7/homework_7_3.py
@@ -0,0 +1,12 @@
+string1 = 'результат операции: 42'
+string2 = 'результат операции: 54'
+string3 = 'результат работы программы: 209'
+string4 = 'результат: 2'
+
+def result_calc(string):
+    return int(string.split()[-1]) + 10
+
+print(result_calc(string1))
+print(result_calc(string2))
+print(result_calc(string3))
+print(result_calc(string4))
