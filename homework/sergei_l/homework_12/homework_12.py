4ec3ee27e2e7094d168336df34d206c1cd274282
humansinc1 <humans.inc@gmail.com>
2025-07-29 23:01:53 +0300

    added a task for homework 12

---
+++ homework/sergei_l/homework_12/homework_12.py
@@ -0,0 +1,59 @@
+class Flowers:
+
+    def __init__(self, name, life_time, cost):
+        self.name = name
+        self.life_time = life_time
+        self.cost = cost
+
+
+class WhiteFlowers(Flowers):
+    color = 'белый'
+    def __init__(self, name, life_time, cost):
+        super().__init__(name, life_time, cost)
+
+
+class RedFlowers(Flowers):
+    color = 'красный'
+    def __init__(self, name, life_time, cost):
+        super().__init__(name, life_time, cost)
+
+
+class Bouquet:
+
+    def __init__(self, *args):
+        self.flower_list = list(args)
+
+    def bouquet_cost(self):
+        total_cost = [x.cost for x in self.flower_list]
+        result = sum(total_cost)
+        return f"Стоимость букета: {result} BYN"
+
+    def bouquet_lifetime(self):
+        lifetime_sum = [x.life_time for x in self.flower_list]
+        result = sum(lifetime_sum) / len(lifetime_sum)
+        return f"Время увядания букета: {round(result, 1)} дней"
+
+    def sort_by_cost(self):
+        flower_cost = [x.cost for x in self.flower_list]
+        return sorted(flower_cost)
+
+    def sort_by_color(self):
+        flower_color = [x.color for x in self.flower_list]
+        return sorted(flower_color)
+
+    def find_by_color(self, color):
+        flower_color = [x.name for x in self.flower_list if x.color == color]
+        return f" {color.capitalize()} цветы в букете это {', '.join(flower_color)}"
+
+
+flower1 = WhiteFlowers('Роза', 7, 13)
+flower2 = RedFlowers('Орхидея',5, 3.5)
+flower3 = WhiteFlowers('Пион', 2,7)
+
+bouquet1 = Bouquet(flower1, flower2, flower3)
+
+print(bouquet1.bouquet_lifetime())
+print(bouquet1.bouquet_cost())
+print(bouquet1.sort_by_cost())
+print(bouquet1.sort_by_color())
+print(bouquet1.find_by_color('красный'))
