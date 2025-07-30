2d4136c096d6b391736d3f30f35304ec2ce8dfab
humansinc1 <humans.inc@gmail.com>
2025-07-30 14:00:27 +0300

    moved color to parent class, modified child classes, modified sort and find methods

--- homework/sergei_l/homework_12/homework_12.py
+++ homework/sergei_l/homework_12/homework_12.py
@@ -1,24 +1,21 @@
 class Flowers:
 
-    def __init__(self, name, life_time, cost):
+    def __init__(self, name, life_time, cost, color):
         self.name = name
         self.life_time = life_time
         self.cost = cost
+        self.color = color
 
 
 class WhiteFlowers(Flowers):
 
     def __init__(self, name, life_time, cost):
-        super().__init__(name, life_time, cost)
-        self.color = 'белый'
-
+        super().__init__(name, life_time, cost, 'белый')
 
 class RedFlowers(Flowers):
 
     def __init__(self, name, life_time, cost):
-        super().__init__(name, life_time, cost)
-        self.color = 'красный'
-
+        super().__init__(name, life_time, cost, 'красный')
 
 class Bouquet:
 
@@ -36,16 +33,13 @@ class Bouquet:
         return f"Время увядания букета: {round(result, 1)} дней"
 
     def sort_by_cost(self):
-        flower_cost = [x.cost for x in self.flower_list]
-        return sorted(flower_cost)
+        return sorted(self.flower_list, key=lambda x: x.cost)
 
     def sort_by_color(self):
-        flower_color = [x.color for x in self.flower_list]
-        return sorted(flower_color)
+        return sorted(self.flower_list, key=lambda x: x.color)
 
     def find_by_color(self, color):
-        flower_color = [x.name for x in self.flower_list if x.color == color]
-        return f" {color.capitalize()} цветы в букете это {', '.join(flower_color)}"
+        return [x for x in self.flower_list if x.color == color]
 
 
 flower1 = WhiteFlowers('Роза', 7, 13)
@@ -58,4 +52,4 @@ print(bouquet1.bouquet_lifetime())
 print(bouquet1.bouquet_cost())
 print(bouquet1.sort_by_cost())
 print(bouquet1.sort_by_color())
-print(bouquet1.find_by_color('красный'))
+print(bouquet1.find_by_color('белый'))
