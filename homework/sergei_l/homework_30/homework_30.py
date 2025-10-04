3d862ccd1ace6cf2b04b4fe18067a4252d06b255
humansinc1 <humans.inc@gmail.com>
2025-10-04 09:00:10 +0300

    removed unnecessary print and add whitespace after function

--- homework/sergei_l/homework_30/homework_30.py
+++ homework/sergei_l/homework_30/homework_30.py
@@ -7,13 +7,13 @@ def test_requests(page: Page):
     def route_handler(route: Route):
         response = route.fetch()
         body = response.json()
-        # print(body['body']['digitalMat'][0]['familyTypes'][0]['productName'])
         body['body']['digitalMat'][0]['familyTypes'][0]['productName'] = 'яблокофон 17 про'
         body = json.dumps(body)
         route.fulfill(
             response=response,
             body=body
         )
+
     page.route(re.compile('digital-mat'), route_handler)
     page.goto('https://www.apple.com/shop/buy-iphone')
     page.get_by_role('heading', name='iPhone 17 Pro &').click()
